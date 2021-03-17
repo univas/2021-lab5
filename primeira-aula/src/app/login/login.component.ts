808995202f315485441e3e2cc40daf2875c50e72
Rodrigo Faria <rodrigolfsi@gmail.com>
2021-03-17 12:59:20 -0300

    Four types of data binding

--- primeira-aula/src/app/login/login.component.ts
+++ primeira-aula/src/app/login/login.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userName: string = 'Rodrigo'
+  className = 'blue'
+  email: string = 'seu_email@gmail.com'
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getName() {
+    return 'UNIVAS 2021'
+  }
+
+  printHello() {
+    if (this.className === 'blue') {
+      this.className = 'green'
+    } else {
+      this.className = 'blue'
+    }
+    alert('Hello!!!')
+  }
+
+  confirmEmail() {
+    if (confirm(`Confirma o email: ${this.email}`)) {
+      alert('email enviado com sucesso!')
+    }
+  }
 }
